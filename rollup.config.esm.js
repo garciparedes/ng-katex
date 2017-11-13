11f77b67a4b19af072b522577b4845245f82affd
Sergio Garcia Prado <sergio@garciparedes.me>
2017-11-13 19:26:48 +0100

    Version v0.3.13

--- rollup.config.esm.js
+++ rollup.config.esm.js
@@ -1,5 +1,7 @@
 import config from './rollup.config.umd.js';
-import {nameLibrary,PATH_DIST} from './config.js'
-config.format = "es";
-config.dest = PATH_DIST + nameLibrary + ".esm.js";
+import { nameLibrary, PATH_DIST } from './config.js'
+config.output = {
+  format: 'es',
+  file: PATH_DIST + nameLibrary + '.esm.js'
+} ;
 export default config;
