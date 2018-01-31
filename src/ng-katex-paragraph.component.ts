3996c7a9d1be4ef697e5502c66cfea5597dc957c
Sergio Garcia Prado <sergio@garciparedes.me>
2018-01-31 15:46:08 +0100

    Improved code readability #22

--- src/ng-katex-paragraph.component.ts
+++ src/ng-katex-paragraph.component.ts
@@ -4,19 +4,39 @@ import * as ko from './ng-katex.options';
 @Component({
   selector: 'ng-katex-paragraph',
   template: `
-    <p><span *ngFor="let part of splitParagraph(paragraph)" [ngSwitch]="classify(part)">
-      <ng-katex *ngSwitchCase="1"  [equation]="clean(part)" [options]="{displayMode: true}"></ng-katex>
-      <ng-katex *ngSwitchCase="2" [equation]="clean(part)"></ng-katex>
-      <span *ngSwitchDefault>{{ part }}</span>
-    </span></p>
+    <p>
+      <span *ngFor="let part of splitParagraph(paragraph)"
+            [ngSwitch]="classify(part)">
+
+        <ng-katex *ngSwitchCase="DISPLAY_MODE"
+                  [equation]="clean(part)" [options]="options"></ng-katex>
+
+        <ng-katex *ngSwitchCase="INLINE_MODE"
+                  [equation]="clean(part)"></ng-katex>
+
+        <span *ngSwitchDefault>{{ part }}</span>
+
+      </span>
+    </p>
   `
 })
 export class KatexParagraphComponent {
 
-  private static readonly DISPLAY_MODE: number = 1;
-  private static readonly INLINE_MODE: number = 2;
-  private static readonly splitRegex = /(\$(?:\\\$|[^\$])+\$)|(\$\$(?:\\\$|[^\$])+\$\$)/g
+  private readonly DISPLAY_MODE: number = 1;
+  private readonly INLINE_MODE: number = 2;
 
+
+  private readonly splitRe = /(\$(?:\\\$|[^\$])+\$)|(\$\$(?:\\\$|[^\$])+\$\$)/g;
+
+  private readonly matchDisplayRe = /(?:\$\$((?:\\\$|[^\$])+)\$\$)/;
+  private readonly matchInlineRe = /(?:\$((?:\\\$|[^\$])+)\$)/;
+
+  private readonly cleanRe = /(\${1,2})((?:\\\$|[^\$])+)\1/
+
+
+  private readonly options: ko.KatexOptions = {
+    displayMode: true
+  };
 
   @Input() paragraph: string;
   @Output() onError = new EventEmitter<any>();
@@ -26,13 +46,13 @@ export class KatexParagraphComponent {
   }
 
   splitParagraph(paragraph: string): Array<string> {
-    return paragraph.split(KatexParagraphComponent.splitRegex).filter(x => x)
+    return paragraph.split(this.splitRe).filter(x => x)
   }
 
   private classify(s: string): number {
-    if (s.match(/(?:\$\$((?:\\\$|[^\$])+)\$\$)/)) {
+    if (s.match(this.matchDisplayRe)) {
       return 1;
-    } else if (s.match(/(?:\$((?:\\\$|[^\$])+)\$)/)) {
+    } else if (s.match(this.matchInlineRe)) {
       return 2;
     } else {
       return 0;
@@ -40,6 +60,6 @@ export class KatexParagraphComponent {
   }
 
   private clean(s: string): string {
-    return s.match(/(\${1,2})((?:\\\$|[^\$])+)\1/)[2];
+    return s.match(this.cleanRe)[2];
   }
 }
