7a656f35eb649c1caf7f6715ec17b3d0149b0b96
Sergio Garcia Prado <sergio@garciparedes.me>
2019-08-26 15:31:00 +0200

    Fixed bug related with #509.

--- src/ng-katex.module.ts
+++ src/ng-katex.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser'
 import { KatexService } from './ng-katex.service';
 import { KatexDirective } from './ng-katex.directive';
 import { KatexComponent } from './ng-katex.component';
@@ -10,7 +9,6 @@ import { KatexHtmlComponent } from './ng-katex-html.component';
 @NgModule({
   imports: [
     CommonModule,
-    BrowserModule,
   ],
   providers: [
     KatexService,
