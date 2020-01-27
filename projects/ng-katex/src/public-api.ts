1c410932e359dfe69964e1a17354ba15c2bf2ed9
Matt Moran <=>
2020-01-26 19:51:50 -0500

    create library project and migrate old source

---
+++ projects/ng-katex/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-katex
+ */
+export * from './lib/ng-katex.options';
+export * from './lib/ng-katex.component';
+export * from './lib/ng-katex-paragraph.component';
+export * from './lib/ng-katex.module';
